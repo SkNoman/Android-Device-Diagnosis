064a82c1e3c95a6edb84f6e1d0d9ec4e49f0efb2
Abdullah Al Noman <39592494+SkNoman@users.noreply.github.com>
2023-03-22 14:54:18 +0600

    add view binding

--- app/src/main/java/com/example/shutdowntime/MainActivity.kt
+++ app/src/main/java/com/example/shutdowntime/MainActivity.kt
@@ -11,20 +11,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import com.example.shutdowntime.databinding.ActivityMainBinding
 import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val shutdownReceiver = object : BroadcastReceiver(){
         @RequiresApi(Build.VERSION_CODES.O)
         override fun onReceive(context: Context?, intent: Intent?) {
             println("Hello world im shutting down")
-            Log.e("hello","See im down")
             val sharedPreferences = context?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
             val editor = sharedPreferences?.edit()
             val currentTime = LocalDateTime.now()
-            Log.e("helloTime",currentTime.toString())
-            editor?.putString("time2", currentTime.toString())
+            println(currentTime.toString())
+            editor?.putString("shutdown_time", currentTime.toString())
             editor?.apply()
         }
     }
@@ -32,25 +33,21 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val shutDownFilter = IntentFilter(Intent.ACTION_SHUTDOWN)
         registerReceiver(shutdownReceiver,shutDownFilter)
 
         val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        val savedTime = sharedPreferences.getString("time", "time")
-        val endTime = sharedPreferences.getString("time2","time2")
 
+        val savedTime = sharedPreferences.getString("boot_time", "")
+        val endTime = sharedPreferences.getString("shutdown_time","")
 
-        // Use the savedTime value as needed
-         Log.d("MainActivity", "Saved time: $savedTime")
+        println("Saved time: $savedTime,$endTime")
 
-        //hooks
-        val txtBootTime = findViewById<TextView>(R.id.txtBootTime)
-        val txtShutdownTime = findViewById<TextView>(R.id.shutdownTime)
-        txtBootTime.text = "Boot Time: " +savedTime.toString()
-        txtShutdownTime.text = "ShutDown Time: "+endTime.toString()
+        binding.txtBootTime.text = "Boot Time: " +savedTime.toString()
+        binding.shutdownTime.text = "ShutDown Time: "+endTime.toString()
 
 
     }
--- app/src/main/java/com/example/shutdowntime/ShutdownReceiver.kt
+++ app/src/main/java/com/example/shutdowntime/ShutdownReceiver.kt
@@ -16,31 +16,16 @@ class ShutdownReceiver : BroadcastReceiver() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context?, intent: Intent?) {
-       // val action = intent?.action
-      //  Log.e("hello",action.toString())
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED){
             println("Hello world im booted up")
-            Log.e("hello","See im booted")
-
             val sharedPreferences = context?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
             val editor = sharedPreferences?.edit()
             val currentTime = LocalDateTime.now()
-            Log.e("helloTime",currentTime.toString())
-            editor?.putString("time", currentTime.toString())
+            println(currentTime.toString())
+            editor?.putString("boot_time", currentTime.toString())
             editor?.apply()
 
         }
-//        else if(intent?.action == ACTION_SHUTDOWN){
-//            println("Hello world im shutting down")
-//            Log.e("hello","See im down")
-//            val sharedPreferences = context?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-//            val editor = sharedPreferences?.edit()
-//            val currentTime = LocalDateTime.now()
-//            Log.e("helloTime",currentTime.toString())
-//            editor?.putString("time2", currentTime.toString())
-//            editor?.apply()
-//        }
-
     }
 
 
